e17d7c34464d98965eb694e1c820ce8f6e39a114
Wizarding Studios <57035573+WizardingStudios@users.noreply.github.com>
2022-01-30 01:33:22 +0100

    Update scroll_date_picker.dart

--- lib/src/scroll_date_picker.dart
+++ lib/src/scroll_date_picker.dart
@@ -254,7 +254,6 @@ class _ScrollDatePickerState extends State<ScrollDatePicker> {
               Expanded(
                 child: Container(
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
                     gradient: LinearGradient(
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter,
@@ -279,7 +278,6 @@ class _ScrollDatePickerState extends State<ScrollDatePicker> {
               Expanded(
                 child: Container(
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
                     gradient: LinearGradient(
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter,
